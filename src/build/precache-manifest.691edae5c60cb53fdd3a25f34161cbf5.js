dc162c4d0f1e3d50432741632c4ca3f456711a3e
Moez Firozbhai Narsidani <c0716607@mylambton.ca>
2019-11-23 17:09:21 -0500

    first

---
+++ src/build/precache-manifest.691edae5c60cb53fdd3a25f34161cbf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db47ff31dfc15d88c7da85e7e3ee86e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31837ebde47fa47e323e",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "f33ea8afebaa14aa3338",
+    "url": "/static/js/2.14852b03.chunk.js"
+  },
+  {
+    "revision": "31837ebde47fa47e323e",
+    "url": "/static/js/main.8d621f69.chunk.js"
+  },
+  {
+    "revision": "35a7acf4fb928453b799",
+    "url": "/static/js/runtime-main.8727d782.js"
+  }
+]);
